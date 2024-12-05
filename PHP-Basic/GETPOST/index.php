5aefa0ce6bb7fe49fb2eba1dc56d46c10c3c0963
Rupesh Tech <103326507+Rupeshkumar8862@users.noreply.github.com>
2024-12-05 16:18:25 +0530

    add basic php file

---
+++ PHP-Basic/GETPOST/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> 7 super Global variable Tutorials</title>
+</head>
+<body>
+    <!-- 
+notes agar ek file data dusre file use krna hai ya dusre page me print krna to wha super global variable use krte hai
+ $_GET ,POST, REQUEST ,SERVER SESSION ,COOKIE ,FILES
+
+  $_get 
+  $_post    1.jada tar form me use krte hai ()
+             ya dusre page print  ya eske url me print (from me input name lena jaruri h)
+          ?? print krenge ??
+            3.database save
+            4.conditianal page content
+
+ -->
+
+<form action="testfrom.php" method="GET">
+    Name: <input type="text" name="fname"> <br> <br>
+    Age: <input type="number" name="age"> <br> <br>
+    <input type="submit" name="save">
+</form>
+
+</body>
+</html>
---
+++ PHP-Basic/GETPOST/testfrom.php
@@ -0,0 +1,30 @@
+<?php
+
+//GET  (FOR URL,SEARCH ,ID)
+// print   
+// es page data aaya wo multiple me array bn kr aa rha h eslie array ko print_r se print krenge
+// echo "<pre>";
+// print_r($_GET);
+// echo "</pre>";
+// ek ek krke dekhna
+// echo $_GET['fname'];
+
+
+// post  (FOR PRIVACY ,SECURTY)
+
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+// ek ek krke dekhna
+// echo $_POST['fname'];
+
+
+//REQUEST  () KE LIE METHOD GET YA POST DONO USE KR SKTE H
+echo "<pre>";
+print_r($_REQUEST);
+echo "</pre>";
+// ek ek krke dekhna
+echo $_REQUEST['fname'];
+
+
+?>
